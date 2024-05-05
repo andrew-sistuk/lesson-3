293d3e3ea4112e2c52930b67f8d6e54c1b6cb268
MykhailoVobolis <mvobolis@gmail.com>
2024-05-05 16:46:37 +0300

    task-3

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,31 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryInfo, Heading, Section } from 'components';
+
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchCountry } from 'service/countryApi';
 
 export const Country = () => {
+  const { countryId } = useParams();
+
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [countryId]);
+
   return (
     <Section>
       <Container>
         <Heading title="SearchCountry" bottom />
+        {country && <CountryInfo {...country} />}
       </Container>
     </Section>
   );
